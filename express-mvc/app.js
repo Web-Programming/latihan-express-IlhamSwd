4fc37be5f41fa97ffa6dd5de11940fc662ecd5cf
IlhamSwd <160827594+IlhamSwd@users.noreply.github.com>
2024-10-23 09:33:01 +0700

    updte

--- express-mvc/app.js
+++ express-mvc/app.js
@@ -10,7 +10,7 @@ require('./app_server/models/db');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
-// Impor modul mahasiswa router
+// pisah router
 var mhsController = require('./app_server/routes/mahasiswa');
 
 var app = express();
@@ -21,9 +21,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'ejs');
 
-// daftarkan route mahasiswa
-app.use('/mahasiswa', mhsController);
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// daftarkan route mahasiswa
+app.use('/mahasiswa', mhsController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
