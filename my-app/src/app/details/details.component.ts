120dea1a3cec539f38c179dfd645f30861418cd6
IlhamSwd <160827594+IlhamSwd@users.noreply.github.com>
2024-11-20 09:39:46 +0700

    update baru form

--- my-app/src/app/details/details.component.ts
+++ my-app/src/app/details/details.component.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'; // untuk membuat form 
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <article >
       <img class="listing-photo" [src]="housingLocation?.photo"
         alt="Exterior photo of {{housingLocation?.name}}"/>
+
       <section class="listing-description">
         <h2 class="listing-heading">{{housingLocation?.name}}</h2>
         <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
@@ -24,6 +27,13 @@ import { HousingLocation } from '../housing-location';
           <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
         </ul>
       </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply Now to live here</h2>
+        <form [formGroup]="applyForm" (submit) = "submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName">
+        </form>
+      </section>
     </article>
   `,
   styleUrl: './details.component.css'
@@ -33,10 +43,17 @@ export class DetailsComponent {
   housingLocationId = 0;
   housingService: HousingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName : new FormControl('')
+  })
+
   constructor() {
     this.housingLocationId = Number(this.route.snapshot.params['id'])
-    this.housingLocation = this.housingService.getHousingLocationById
-    (this.housingLocationId)
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId)
     console.log(this.housingLocation)
   }
+  submitApplication() {
+    console.log(this.applyForm.value.firstName)
+  }
 }
+
